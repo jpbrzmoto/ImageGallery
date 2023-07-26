55c9270e481568e52f26a5ebcf66d4a738ccbf39
jpbrzmoto <jpbrzmoto@gmail.com>
2023-07-26 20:09:46 -0300

    fix3

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import Principal from './pages/principal.tsx'
+import './src/index.css'
+import Principal from './src/pages/principal'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
